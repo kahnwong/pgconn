8e56ca4e1b768dac66b714632aef2bbbf13ef087
kahnwong <karn@karnwong.me>
2023-08-25 14:32:17 +0700

    add autocomplete

---
+++ pg_conn_cli/utils/autocomplete.py
@@ -0,0 +1,13 @@
+import typer
+
+from pg_conn_cli.utils.config import db_role_mapping
+
+
+def complete_db():  # noqa
+    return list(db_role_mapping.keys())
+
+
+def complete_role(ctx: typer.Context):  # noqa
+    database = ctx.params.get("database")
+
+    return db_role_mapping[database]
