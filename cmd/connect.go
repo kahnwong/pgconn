5154fc70825da9a9034ed6f1107b5f0c0955e8ea
Karn Wong <karn@karnwong.me>
2024-06-18 16:23:42 +0700

    extract to utils_db.go

--- cmd/connect.go
+++ cmd/connect.go
@@ -7,8 +7,6 @@ import (
 	"os/exec"
 	"syscall"
 
-	"github.com/fatih/color"
-
 	"github.com/spf13/cobra"
 )
 
@@ -82,46 +80,6 @@ func (c *ConnectCommand) Synopsis() string {
 }
 
 // functions
-func ConnectDB(c Connection) *exec.Cmd {
-	// check if pgcli exists
-	binaryName := "pgcli"
-	_, err := exec.LookPath(binaryName)
-	if err != nil {
-		fmt.Printf("Binary '%s' not found in the PATH\n", binaryName)
-		os.Exit(1)
-	}
-
-	// set hostname
-	var connectHostname string
-	if c.ProxyKind != "" {
-		if c.ProxyKind == "ssh" {
-			connectHostname = "localhost"
-		} else if c.ProxyKind == "cloud-sql-proxy" {
-			connectHostname = "127.0.0.1"
-		}
-	} else {
-		connectHostname = c.Hostname
-	}
-
-	// print port
-	green := color.New(color.FgGreen).SprintFunc()
-	fmt.Printf("Port: %s\n", green(c.Port))
-
-	// connect
-	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", c.Username, c.Password, connectHostname, c.Port, c.Dbname)
-	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("pgcli \"%s\"", connectionString))
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	if err := cmd.Run(); err != nil {
-		fmt.Printf("Failed to start the second process: %v\n", err)
-		os.Exit(1)
-	}
-
-	return cmd
-}
-
 func cleanup(cmd *exec.Cmd) {
 	pgid, err := syscall.Getpgid(cmd.Process.Pid)
 	if err == nil {
---
+++ cmd/utils_db.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/fatih/color"
+)
+
+func ConnectDB(c Connection) *exec.Cmd {
+	// check if pgcli exists
+	binaryName := "pgcli"
+	_, err := exec.LookPath(binaryName)
+	if err != nil {
+		fmt.Printf("Binary '%s' not found in the PATH\n", binaryName)
+		os.Exit(1)
+	}
+
+	// set hostname
+	var connectHostname string
+	if c.ProxyKind != "" {
+		if c.ProxyKind == "ssh" {
+			connectHostname = "localhost"
+		} else if c.ProxyKind == "cloud-sql-proxy" {
+			connectHostname = "127.0.0.1"
+		}
+	} else {
+		connectHostname = c.Hostname
+	}
+
+	// print port
+	green := color.New(color.FgGreen).SprintFunc()
+	fmt.Printf("Port: %s\n", green(c.Port))
+
+	// connect
+	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable", c.Username, c.Password, connectHostname, c.Port, c.Dbname)
+	cmd := exec.Command("/bin/sh", "-c", fmt.Sprintf("pgcli \"%s\"", connectionString))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Failed to start the second process: %v\n", err)
+		os.Exit(1)
+	}
+
+	return cmd
+}
